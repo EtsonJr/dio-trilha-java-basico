2a7d69a5e1bbe2c6e04d6f75e5e085e9beba969b
Etson <etsonmo@gmail.com>
2024-08-10 04:54:58 -0300

    ContaTerminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class ContaTerminal{
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+    public static void main(String[] args){
+        try (Scanner Scanner = new Scanner(System.in)) {
+            System.out.println("Digite o Usuário:");
+            int usuario = Scanner.nextInt(); //Para numeros deve se usar 'int' e a variavel seguida de scanner.nextInt
+
+            System.out.println("Agência:");// Agencia está pulando, não recebe entrada do usuário.
+            String agencia = Scanner.nextLine();
+
+            System.out.println("Digite Seu Nome:");// Funciona!
+            String nomeCliente = Scanner.nextLine();
+
+            System.out.println("Digite seu saldo:");//Funciona!
+            double saldo = Scanner.nextDouble();
+
+            System.out.println("Usuário:" + usuario);
+
+            System.out.println("Agência:" + agencia);
+
+            System.out.println("Nome:" + nomeCliente);
+
+            System.out.println("Saldo:" + saldo);
+            
+        }
+        }
+        }
