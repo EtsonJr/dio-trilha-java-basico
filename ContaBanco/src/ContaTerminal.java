4576090b14795acebe3f435f92f8979bbc352852
Etson <etsonmo@gmail.com>
2024-08-22 01:30:40 -0300

    Ok

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -3,26 +3,20 @@ import java.util.Scanner;
 public class ContaTerminal{
     public static void main(String[] args){
         try (Scanner Scanner = new Scanner(System.in)) {
-            System.out.println("Digite o Usuário:");
-            int usuario = Scanner.nextInt(); //Para numeros deve se usar 'int' e a variavel seguida de scanner.nextInt
-
-            System.out.println("Agência:");// Agencia está pulando, não recebe entrada do usuário.
+            System.out.println("Digite sua conta:");
+            int contaNum = Scanner.nextInt();
+            System.out.println("Agência:");
             String agencia = Scanner.nextLine();
+            agencia = Scanner.nextLine();
 
-            System.out.println("Digite Seu Nome:");// Funciona!
+            System.out.println("Digite Seu Nome:");
             String nomeCliente = Scanner.nextLine();
 
-            System.out.println("Digite seu saldo:");//Funciona!
+            System.out.println("Digite seu saldo:");
             double saldo = Scanner.nextDouble();
 
-            System.out.println("Usuário:" + usuario);
+            System.out.println("Olá " + nomeCliente +", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta "+ contaNum +" E seu saldo de R$"+ saldo +" Já está disponivel para saque.");
 
-            System.out.println("Agência:" + agencia);
-
-            System.out.println("Nome:" + nomeCliente);
-
-            System.out.println("Saldo:" + saldo);
-            
         }
         }
         }
